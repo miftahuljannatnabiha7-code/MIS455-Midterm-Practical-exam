a87747169093bda4bcbe97088f008a6b70f64cff
Miftahul jannat <miftahuljannatnabiha7@gmail.com>
2026-07-04 20:00:24 +0600

    Eighth Commit

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function showContactMessage() {
     var name = document.getElementById("customerName").value ;
     alert("Thank you, " + name + "! We will contact you soon.") ;
 }
+
+
+function CalculateDiscount() {
+    var amount = parseInt(document.getElementById("amount").value);
+    var discount ;
+
+    if (amount >= 1000) {
+        discount = amount * 0.10;
+    }
+
+
+}
